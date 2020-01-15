9e0c7003768836cf30b791ed807a48ad3e2e7e0c
jslee <jslee02@korens.co.kr>
2020-01-15 15:56:51 +0900

    :cloud: aws-sequelize

--- lib/config/config.ts
+++ lib/config/config.ts
@@ -3,22 +3,24 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_Dialect || undefined,
-    operatorsAliases: false,
     jwtSecret: "@tsback",
+    local: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_Dialect || undefined,
+        operatorsAliases: false,
+    },
+    aws: {
+        username: process.env.AWS_DB_USERNAME,
+        password: process.env.AWS_DB_PASSWORD,
+        database: process.env.AWS_DB_NAME,
+        host: process.env.AWS_DB_HOST,
+        dialect: process.env.AWS_DB_Dialect,
+        operatorsAliases: false,
+    },
 };
-// test: {
-//   username: "root",
-//   password: null,
-//   database: "database_test",
-//   host: "127.0.0.1",
-//   dialect: "mysql",
-//   operatorsAliases: false,
-// },
 // production: {
 //   username: "root",
 //   password: null,
--- lib/sequelize.ts
+++ lib/sequelize.ts
@@ -2,14 +2,18 @@ import { resolve } from "path";
 import { Sequelize } from "sequelize-typescript";
 import config from "./config/config";
 
+const aws = config.aws;
 const sequelize = new Sequelize({
-    database: config.database,
-    dialect: "postgres" || config.dialect,
-    host: config.host,
-    username: config.username,
-    password: config.password,
+    database: aws.database,
+    dialect: "postgres" || aws.dialect,
+    host: aws.host,
+    username: aws.username,
+    password: aws.password,
     storage: ":memory:",
     models: [__dirname + "/models"],
     logging: false,
+    dialectOptions: {
+        connectTimeout: 60000,
+    },
 });
 export default sequelize;
